6d7014c4e1ddb207abef89c6f72f477f20dcddf5
Dbakh <dbakhshpour@gmail.com>
2017-02-24 13:21:05 -0700

    Takes a string input and filters it ezpz

--- src/textFileReader.java
+++ src/textFileReader.java
@@ -4,35 +4,15 @@ import java.util.*;
 
 public class textFileReader {
 	
-	public static HashSet<String> filterText(String inputText)
+	public static String filterText(String inputText)
 	{
-		String temp = null;
-		HashSet<String> input = new HashSet<String>();
+		String temp = inputText;
 		
-		try {
-			
-			FileReader fileReader = new FileReader(inputText);
-			BufferedReader reader = new BufferedReader(fileReader);
-			
-			//Stores input.txt into an array named input
-			while ((temp = reader.readLine()) != null) {
-				//Replaces non-letter characters with ""
-				temp = temp.replaceAll("\\d", "");
-				temp = temp.replaceAll("\\W", "");
-				//Adds filtered input.txt to arraylist called input
-				if (!temp.isEmpty()) {
-					input.add(temp);
-				}
-			}
-			reader.close();
-		}
-		catch(FileNotFoundException ex) {
-			System.out.println(inputText + " " + "not found.");
-		}
-		catch(IOException ex) {
-			System.out.println("Unable to read " + inputText);
-		}
-		return input;
+		//Replaces non-letter characters with ""
+		temp = temp.replaceAll("\\d", "");
+		temp = temp.replaceAll("\\W", "");
+		
+		return temp;
 	}
 }
 
