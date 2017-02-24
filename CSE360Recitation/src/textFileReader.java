220a200335a833e2430bcbe65a3a36140053eac1
weirdestnerd <olu1995@hotmail.com>
2017-02-24 14:48:44 -0700

    Moved files around

---
+++ CSE360Recitation/src/textFileReader.java
@@ -0,0 +1,17 @@
+import java.io.*;
+import java.util.*;
+
+
+public class textFileReader {
+	
+	public static String filterText(String inputText)
+	{
+		String temp = inputText;
+		
+		//Replaces non-letter characters with ""
+		temp = temp.replaceAll("\\d", "");
+		temp = temp.replaceAll("\\W", "");
+		
+		return temp;
+	}
+}
